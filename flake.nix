c115d38ec55dbfa11458551237dec8c0ec650811
Rob Ede <robjtede@icloud.com>
2025-08-31 00:06:23 +0100

    chore: update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
     x52 = {
       url = "github:x52dev/nix";
@@ -30,8 +30,6 @@
               config.formatter
             ] ++ lib.optional pkgs.stdenv.isDarwin [
               pkgs.pkgsBuildHost.libiconv
-              pkgs.pkgsBuildHost.darwin.apple_sdk.frameworks.CoreFoundation
-              pkgs.pkgsBuildHost.darwin.apple_sdk.frameworks.Security
             ];
 
             shellHook = ''
